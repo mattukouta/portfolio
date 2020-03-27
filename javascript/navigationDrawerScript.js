e1116c2aaf5fe93a23c36a2b3729c018eb9dd56c
Kouta Takamatsu <bb1016181@gmail.com>
2020-03-27 10:12:02 +0900

    select背景の修正

--- javascript/navigationDrawerScript.js
+++ javascript/navigationDrawerScript.js
@@ -1,38 +1,64 @@
 function drawerClose(pageName) {
+    // Navigation Drawerを閉じる
     document.getElementById('navInput').checked = false;
 
-    var introductionPage = document.getElementById("introductionContent")
-    var activityPage = document.getElementById("activityContent")
-    var productionPage = document.getElementById("productionContent")
-    var linkPage = document.getElementById("linkContent")
+    // document.getElementById(pageName + 'BackgroundColor').style.backgroundColor = '#5ab4bd';
+
+
+    // 表示するContentの切り替え
+    var introductionPage = document.getElementById("introductionContent");
+    var activityPage = document.getElementById("activityContent");
+    var productionPage = document.getElementById("productionContent");
+    var linkPage = document.getElementById("linkContent");
+
+    var introductionBackgroundColor = document.getElementById('introductionBackgroundColor');
+    var activityBackgroundColor = document.getElementById('activityBackgroundColor');
+    var productionBackgroundColor = document.getElementById('productionBackgroundColor');
+    var linkBackgroundColor = document.getElementById('linkBackgroundColor');
 
     switch (pageName) {
         case "introduction":
             introductionPage.style.display = 'block';
+            introductionBackgroundColor.style.backgroundColor = '#5ab4bd';
             activityPage.style.display = 'none';
+            activityBackgroundColor.style.backgroundColor = '#363434';
             productionPage.style.display = 'none';
+            productionBackgroundColor.style.backgroundColor = '#363434';
             linkPage.style.display = 'none';
+            linkBackgroundColor.style.backgroundColor = '#363434';
             break;
 
         case "activity":
             introductionPage.style.display = 'none';
+            introductionBackgroundColor.style.backgroundColor = '#363434';
             activityPage.style.display = 'block';
+            activityBackgroundColor.style.backgroundColor = '#5ab4bd';
             productionPage.style.display = 'none';
+            productionBackgroundColor.style.backgroundColor = '#363434';
             linkPage.style.display = 'none';
+            linkBackgroundColor.style.backgroundColor = '#363434';
             break;
 
         case "production":
             introductionPage.style.display = 'none';
+            introductionBackgroundColor.style.backgroundColor = '#363434';
             activityPage.style.display = 'none';
+            activityBackgroundColor.style.backgroundColor = '#363434';
             productionPage.style.display = 'block';
+            productionBackgroundColor.style.backgroundColor = '#5ab4bd';
             linkPage.style.display = 'none';
+            linkBackgroundColor.style.backgroundColor = '#363434';
             break;
 
         case "link":
             introductionPage.style.display = 'none';
+            introductionBackgroundColor.style.backgroundColor = '#363434';
             activityPage.style.display = 'none';
+            activityBackgroundColor.style.backgroundColor = '#363434';
             productionPage.style.display = 'none';
+            productionBackgroundColor.style.backgroundColor = '#363434';
             linkPage.style.display = 'block';
+            linkBackgroundColor.style.backgroundColor = '#5ab4bd';
             break;
     }
 }
